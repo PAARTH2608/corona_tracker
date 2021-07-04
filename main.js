21c62e466dfaaff5f8b6e219ca066384675c767a
PAARTH2608 <76266935+PAARTH2608@users.noreply.github.com>
2021-07-05 02:16:44 +0530

    added search by state

--- main.js
+++ main.js
@@ -1,29 +1,32 @@
 const myform1 = document.getElementById('myform1');
 const country = document.getElementById('country');
+const province = document.getElementById('state');
 myform1.addEventListener('submit', function (e){
     e.preventDefault();
     var count = country.value;
-
+    var prov = province.value;
+    
     //fetching
     const url = 'https://api.covid19api.com/live/country/'+`${count}`+'/status/confirmed'
     fetch(url)
     .then((data) => data.json())
     .then((data) => {
-        console.log(data);
+        // console.log(data);
         const index = data.length - 1;
-        const active = document.getElementById('active');
-        const confirmed = document.getElementById('confirmed');
-        const recovered = document.getElementById('recovered');
-        const deaths = document.getElementById('deaths');
-
-        active.append(data[index].Active);
-        confirmed.append(data[index].Confirmed);
-        recovered.append(data[index].Recovered);
-        deaths.append(data[index].Deaths);
-        country.value = "";
+        for(var i=data.length-1;i>=0;i--){
+            if(data[i].Province === prov){
+                // console.log(data[i].Province)
+                // console.log(i)
+                active.append(data[i].Active);
+                confirmed.append(data[i].Confirmed);
+                recovered.append(data[i].Recovered);
+                deaths.append(data[i].Deaths);
+                country.value = "";
+                province.value = "";
+                break
+            }
+        }
     })
-
-
     country.addEventListener('click',  () => {
         window.location.reload();
     })
